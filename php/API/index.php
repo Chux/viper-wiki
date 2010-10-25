7037dd47f4e614d2947cebc523edbb53dd0eb044
Irina <Irina@.(none)>
2010-10-25 15:47:04 +0200

    Tested and changed APIController so that it can work with Viktor's mod_rewrite

--- php/API/index.php
+++ php/API/index.php
@@ -1,5 +1,16 @@
-<pre>
 <?php
+
+require_once 'APIController.class.php';
+//require_once 'ArticleHandler.class.php';
+
+echo "<pre>";
 var_dump($_GET);
+echo "</pre>";
+
+
+$ac = new APIController();
+$ac->sendToHandler();
+
+
+
 ?>
-</pre>
