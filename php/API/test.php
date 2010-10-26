45bda6b91a8edc63446ca04d4ca136d232a5b02c
Tim Jakobsson <tim.jakobsson@gmail.com>
2010-10-26 15:44:19 +0200

    wikiSyntaxConverter.class.php tested and done comments added

--- php/API/test.php
+++ php/API/test.php
@@ -1,15 +1,17 @@
 <?php
-	require_once('wikiSyntaxConverter.class.php');
 
-$wsc = new WikiSyntaxConverter();
-print_r($wsc);
+require('wikiSyntaxConverter.class.php');
 
+$result = WikiSyntaxConverter::convertToHTML($_POST['wikisyntax']);
+print_r($result); 
+
+	
 echo "<pre>";
 var_dump($_GET);
 echo "</pre>";
 
-echo "<form name='wiki' action='wikiSyntaxConverter.class.php' method='get'>
-	 wikiSomething:<textarea name='wiki' COLS=40 ROWS=6></TEXTAREA>
+echo "<form name='wiki' action='' method='POST'>
+	 wikiSomething:<textarea name='wikisyntax' COLS=40 ROWS=6></TEXTAREA>
 	 <P><input type=submit value='submit'> 
 	 </form>"
 
