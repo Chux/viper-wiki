dbe36a03298a96e66b89dcc19067b766fce1484b
Chux <lechux@gmail.com>
2010-10-29 11:36:35 +0200

    Java project:PreDev. Created frontend for the project, setup.js need to be configured

---
+++ java/frontend/js/ArticleModel.js
@@ -0,0 +1,25 @@
+ArticleModel.prototype = new Model();
+ArticleModel.prototype.constructor = ArticleModel;
+
+function ArticleModel() {
+	Model.call(this);
+	var resourceType = 'Article';
+	var content;
+	var title;
+	var dateTime;
+	var userId;
+	var id;
+	
+	function getResourceType(){
+		return resourceType;
+	}
+	
+	var object = {
+		content : content,
+		dateTime : dateTime,
+		userId : userId,
+		getResourceType:getResourceType 
+	};
+	
+	return object;
+}
