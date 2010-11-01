52c0c381a296da44facc457a1386462299a14783
Tim Jakobsson <tim.jakobsson@gmail.com>
2010-11-01 15:06:30 +0100

    removed the .REMOTE .BASE .LOCAL .BACKUP file to the ApiServlet.java

--- java/src/viper/ApiServlet.java
+++ java/src/viper/ApiServlet.java
@@ -15,32 +15,31 @@ import javax.servlet.http.HttpSession;
 import viper.entities.Article;
 import viper.entities.ArticleDAO;
 
+
 /**
  * Servlet implementation class ApiServlet
- * 
  * @param <viperSession>
  */
 public class ApiServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * Default constructor.
-	 */
-	public ApiServlet() {
-		// super();
-	}
-
+    /**
+     * Default constructor. 
+     */
+    public ApiServlet() {
+    	//super();
+    }
+    
 	public void init(ServletContext servletContext) {
-		File configfile = new File(servletContext
-				.getRealPath("/WEB-INF/hibernate/hibernate.cfg.xml"));
+		File configfile = new File(servletContext.getRealPath("/WEB-INF/hibernate/hibernate.cfg.xml"));
 		viper.db.HibernateUtil.initHibernate(configfile);
 	}
-
+	
 	private viperSession getViperSession(HttpServletRequest request) {
 		HttpSession session = request.getSession();
-
-		viperSession vs = (viperSession) session.getAttribute("session");
-		if (vs == null) {
+		
+		viperSession vs = (viperSession) session.getAttribute("session");  
+		if(vs == null) {
 			vs = new viperSession();
 			ArrayList<Article> articlesDb = new ArrayList<Article>();
 			vs.setAllArticles(articlesDb);
@@ -48,44 +47,36 @@ public class ApiServlet extends HttpServlet {
 		}
 		return vs;
 	}
-
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-	 *      response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		init(this.getServletContext());
 		viperSession vs = getViperSession(request);
-
-		request.getRequestDispatcher("WEB-INF/jsp/Article.jsp").include(
-				request, response);
-
+		
+		request.getRequestDispatcher("WEB-INF/jsp/Article.jsp").include(request, response);
+		
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-	 *      response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
-		// init(this.getServletContext());
-
-		/*String title = request.getParameter("title");
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//init(this.getServletContext());		
+		
+		String title = request.getParameter("title");
 		String content = request.getParameter("content");
-
+		
 		Article na = new Article();
 		na.setTitle(title);
 		na.setContent(content);
-
+		
 		ArticleDAO.saveArticle(na);
-
+		
 		viperSession vs = getViperSession(request);
 		vs.setAllArticles(ArticleDAO.getAllArticles());
-
-		request.getRequestDispatcher("WEB-INF/jsp/Article.jsp").include(
-				request, response);
-				*/
+		
+		request.getRequestDispatcher("WEB-INF/jsp/Article.jsp").include(request, response);
 	}
 
 }
