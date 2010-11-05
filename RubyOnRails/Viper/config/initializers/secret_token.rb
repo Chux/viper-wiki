d6fe6c095a1da92a3f48dd5915d648831965b289
Tim Jakobsson <tim.jakobsson@gmail.com>
2010-11-05 11:19:04 +0100

    MMF1:1 initiated awesome Viper Rails project. Added some supposedly good things to the automagic generated .gitgnore file scaffolded the Article and migrated the db file. All set to pull.

---
+++ RubyOnRails/Viper/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Viper::Application.config.secret_token = 'e43750e86c6019c36558296d69cfc8ede912d7fe76d9ed65015363f3e9efd288cae836ebb6809e85f14a9569a6e47443a598e8de623d391f55d787d58a27b8f7'
