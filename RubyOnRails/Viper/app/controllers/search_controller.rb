27f135dd40dcb0a9bc55c51e64c49ab424a55bbf
Olle Svensson <bolliolle@gmail.com>
2010-11-10 13:59:09 +0100

    mmf4:1 display part of content in search result + ... in front an end

--- RubyOnRails/Viper/app/controllers/search_controller.rb
+++ RubyOnRails/Viper/app/controllers/search_controller.rb
@@ -1,11 +1,26 @@
 class SearchController < ApplicationController
   def index
-    article = Article.search(params[:search_text])
-    unless article  
-      @msg = "create this article? <a href='/article/new/#{params[:search_text]}'"  
-    else
-      @article = article
+    articles = Article.search(params[:search_text])
+    displayChars = 200
+    articles.each do |article|
+      pro = ''
+      post = ''
+      matchIndex =  article.content.index(params[:search_text])
+      if (matchIndex-displayChars/2 > 0)   
+        from = matchIndex - displayChars/2 
+        pro = '... '
+        if (matchIndex+(displayChars/2)) < article.content.length
+          post = ' ...'
+        end
+      else 
+        from = 0
+        if displayChars < article.content.length
+          post =' ...'
+        end
+      end
+      displayChars = 200
+      article.content = pro + article.content.slice(from,displayChars) + post
     end
+    @article = articles
   end
-
 end
